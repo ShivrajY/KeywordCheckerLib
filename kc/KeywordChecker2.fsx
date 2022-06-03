93c0315f08dd1515785fbb1d935906a249c8ebf7
shiv <suprememind@live.com>
2022-06-03 16:53:25 +0530

    .

--- kc/KeywordChecker2.fsx
+++ kc/KeywordChecker2.fsx
@@ -11,7 +11,11 @@ open System.Threading
 open PuppeteerSharp
 open System.IO
 open AngleSharp
+open AngleSharp.Dom
+open AngleSharp.Io
 open AngleSharp.Html.Parser
+open System.Net
+open System.Net.Http
 (******************************************************************************)
 let columnName = "companyhomepageurl"
 
@@ -38,15 +42,14 @@ type RequestGate(n: int) =
                 return! failwith "Semaphore couldn't be aquired..."
 
         }
-
-// How many pages at a time?
-let numberOfPages = 5
-
+//Number of simultaneous pages
+let numberOfPages = 3
 let webRequestGate = RequestGate(numberOfPages)
 
 let tick = "\u2713"
 let cross = "\u1763"
 
+//Constants
 let color = Console.ForegroundColor
 let quote = '"'
 let wordBoundary = @"\b"
@@ -54,12 +57,11 @@ let wordBoundary = @"\b"
 Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 let currentDirectory = __SOURCE_DIRECTORY__
 let downloadPath = Path.Combine(currentDirectory, "Chromium")
-
 let config = Configuration.Default.WithDefaultLoader()
 
-///<summary>
-/// Setup PuppeteerSharp
-///</summary>
+/// <summary>
+/// Setup Chrome options
+/// </summary>
 let SetUpChromeAndGetBrowser () =
     async {
         Console.WriteLine($"Attemping to set up puppeteer to use Chromium found under directory {downloadPath} ")
@@ -87,34 +89,33 @@ let SetUpChromeAndGetBrowser () =
             return! failwith "Couldn't find Chromium executable"
     }
 
-///<summary>
-/// Commandline arguments
-/// </summary>
+//Arguments
 let args = fsi.CommandLineArgs |> Array.tail
 
-///<summary>
-/// Get language using regular RegularExpressions
+/// <summary>
+/// Setup the browser
 /// </summary>
+/// <returns>Page language</returns>
+/// <param name="html">page html</param>
 let getLanguage (html: string) =
     async {
-        //try
-        let m =
-            Regex.Match(
-                html,
-                "lang=\"(.*?)\"",
-                RegexOptions.IgnoreCase
-                ||| RegexOptions.Singleline
-            )
+        if not (String.IsNullOrEmpty(html)) then
+            let m =
+                Regex.Match(
+                    html,
+                    "lang=\"(.*?)\"",
+                    RegexOptions.IgnoreCase
+                    ||| RegexOptions.Singleline
+                )
 
-        if (m.Success) then
-            return m.Groups[1].Value
+            if (m.Success) then
+                return m.Groups[1].Value
+            else
+                return String.Empty
         else
-            return ""
+            return String.Empty
     }
 
-///<summary>
-/// Inputs to be parsed data type
-/// </summary>
 type InputToParse =
     | Attribute of tag: string * attribute: string
     | Meta of name: string
@@ -123,41 +124,43 @@ let pageLanguage = Attribute(tag = "html", attribute = "lang")
 let metaDescription = Meta(name = "description")
 
 /// <summary>
-/// Parses the input to determine the language of the page as well as
-/// meta description
+/// Parse the page
 /// </summary>
 let parseData (html: string) (inputsToParse: InputToParse list) =
     async {
-        use context = BrowsingContext.New(config)
-        let parser = context.GetService<IHtmlParser>()
-        use! doc = parser.ParseDocumentAsync(html) |> Async.AwaitTask
+        if not (String.IsNullOrEmpty(html)) then
+            use context = BrowsingContext.New(config)
+            let parser = context.GetService<IHtmlParser>()
+            use! doc = parser.ParseDocumentAsync(html) |> Async.AwaitTask
 
-        let data =
-            inputsToParse
-            |> List.map (fun input ->
-                match input with
-                | Attribute (tag, attribute) ->
-                    let a = doc.QuerySelector($"{tag}[{attribute}]")
+            let data =
+                inputsToParse
+                |> List.map (fun input ->
+                    match input with
+                    | Attribute (tag, attribute) ->
+                        let a = doc.QuerySelector($"{tag}[{attribute}]")
 
-                    if (a = null) then
-                        String.Empty
-                    else
-                        let v = a.GetAttribute(attribute)
-                        if (v = null) then String.Empty else v
-                | Meta (name) ->
-                    let m = doc.QuerySelector($"meta[name={quote}{name}{quote}]")
+                        if (a = null) then
+                            String.Empty
+                        else
+                            let v = a.GetAttribute(attribute)
+                            if (v = null) then String.Empty else v
+                    | Meta (name) ->
+                        let m = doc.QuerySelector($"meta[name={quote}{name}{quote}]")
 
-                    if (m = null) then
-                        String.Empty
-                    else
-                        let v = m.GetAttribute("content")
-                        if (v = null) then String.Empty else v)
+                        if (m = null) then
+                            String.Empty
+                        else
+                            let v = m.GetAttribute("content")
+                            if (v = null) then String.Empty else v)
 
-        return data
+            return data
+        else
+            return []
     }
 
 /// <summary>
-/// Get DOM Html from chrome browser
+/// Get page DOM html
 /// </summary>
 let fetchUrl (browser: Browser) (url: string) =
     async {
@@ -183,28 +186,26 @@ let fetchUrl (browser: Browser) (url: string) =
     }
 
 /// <summary>
-/// Check the presence of the word in document
+/// Checks words inside DOM
 /// </summary>
 let checkWords (html: string) =
-    async {
-        if (String.IsNullOrWhiteSpace(html)) then
-            let wordsFound =
-                words
-                |> Set.filter (fun word ->
-                    Regex.IsMatch(
-                        html,
-                        $"{wordBoundary}{word}{wordBoundary}",
-                        RegexOptions.IgnoreCase
-                        ||| RegexOptions.Singleline
-                    ))
+    if not (String.IsNullOrWhiteSpace(html)) then
+        let wordsFound =
+            words
+            |> Set.filter (fun word ->
+                Regex.IsMatch(
+                    html,
+                    $"{wordBoundary}{word}{wordBoundary}",
+                    RegexOptions.IgnoreCase
+                    ||| RegexOptions.Singleline
+                ))
 
-            return String.Format("{0}{1}{0}", quote, (String.Join(", ", wordsFound)))
-        else
-            return "\"\""
-    }
+        String.Format("{0}{1}{0}", quote, (String.Join(", ", wordsFound)))
+    else
+        "\"\""
 
 /// <summary>
-/// Create CSV object from csv file
+/// Create CSV object
 /// </summary>
 let createCsv file =
     if (String.IsNullOrEmpty(file)) then
@@ -213,11 +214,47 @@ let createCsv file =
         let csv = CsvFile.Load(file, ignoreErrors = true)
         csv
 
+// To handle Server certificate errors
+ServicePointManager.ServerCertificateValidationCallback <- (fun sender certificate chain sslPolicyErrors -> true)
+
+let handler = new HttpClientHandler()
+handler.AllowAutoRedirect <- true
+let httpClient = new HttpClient(handler)
+
+httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"
+)
+
+/// <summary>
+/// Create url based on httpClient redirect
+/// </summary>
+let createUrl (link: string) =
+    async {
+        let url =
+            if (link.StartsWith("http")) then
+                link
+            else
+                $"https://www.{link}"
+
+        try
+            use! response =
+                httpClient.GetAsync(url, HttpCompletionOption.ResponseHeadersRead)
+                |> Async.AwaitTask
+
+            if (response.IsSuccessStatusCode) then
+                let x = response.RequestMessage.RequestUri.ToString()
+                return x
+            else
+                return $"http://www.{link}"
+        with
+        | _ -> return String.Empty
+    }
+
 let mutable counter = 0
 let locker = obj ()
 
 /// <summary>
-/// Main function to find words
+/// Find words
 /// </summary>
 let findWords (browser: Browser) (csv: CsvFile) (newFile: string) =
     counter <- 0
@@ -245,9 +282,10 @@ let findWords (browser: Browser) (csv: CsvFile) (newFile: string) =
         |> List.mapi (fun index row ->
             async {
                 try
-                    let website = row[ columnName ].Trim()
-                    let! html = fetchUrl browser website
-                    let! words = checkWords (html)
+                    let website = row[columnName]
+                    let! url = createUrl (website)
+                    let! html = fetchUrl browser (url)
+                    let words = checkWords (html)
                     let! language = getLanguage (html)
                     let! data = parseData (html) (pageLanguage :: metaDescription :: [])
 
@@ -306,10 +344,10 @@ let findWords (browser: Browser) (csv: CsvFile) (newFile: string) =
                     return index, str
                 with
                 | _ -> return index, String.Empty
-            } // async
-        ) // mapi
+            })
 
     let results = tasks |> Async.Parallel |> Async.RunSynchronously
+
     browser.CloseAsync() |> ignore
 
     let sortedResult = results |> Array.sortBy fst |> Array.map snd
@@ -323,7 +361,7 @@ let findWords (browser: Browser) (csv: CsvFile) (newFile: string) =
     Console.ForegroundColor <- color
 
 /// <summary>
-/// Work begins here!
+/// Work begins here
 /// </summary>
 let work () =
     match args with
